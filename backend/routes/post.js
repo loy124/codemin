b187aa50eadb2035c8e0b114f77e74c84c1ced02
LEE ON YOU <dhsdb1541@naver.com>
2020-09-03 11:16:19 +0900

    이미지, 옵션 업데이트하기 완료

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -5,10 +5,151 @@ const fs = require("fs");
 const router = express.Router();
 
 // 전체 게시글 얻기
-router.get("/", (req, res) => {
-  return res.json({ hello: "post" });
+router.get("/", async (req, res) => {
+  const data = await Room.findAll({
+    include: [{ model: Option }, { model: Image }],
+  });
+  console.log(data);
+  return res.json({ room: data });
 });
 // 본인이 올린 게시글 얻기
+router.get("/:id", async (req, res) => {
+  const data = await Room.findOne({
+    include: [{ model: Option }, { model: Image }],
+    where: {
+      id: req.params.id,
+    },
+  });
+  console.log(data);
+  return res.json({ room: data });
+});
+
+// TODO patch로 만들어야하는 로직
+// 1. 옵션 업데이트하기
+// 2. 파일 업데이트하기
+// 3. 게시글 내용 수정하기
+
+router.patch("/:id", async (req, res) => {
+  // const data = await Room.update()
+  // 필요한 로직
+  try {
+    const data = await Room.findOne({
+      include: [{ model: Option }, { model: Image }],
+      where: {
+        room_id: req.params.id,
+      },
+    });
+    // console.log(data);
+    // const d = await data.Option.destroy({
+    //   where: {
+    //     id: data[seller_id],
+    //   },
+    // });
+    // console.log(d);
+    // 수행해야하는 로직
+    // 옵션을 다 지우고 새로 추가한다.
+    //
+    // data.Model.update()
+    // console.log(data.getImage());
+    // data.Option
+    return res.json({ room: data });
+  } catch (error) {
+    console.error(error);
+    return res.json({ room: false });
+  }
+  // findOne으로 업데이트하기
+  // 파일을 찾아서 업데이트해줌과 동시에
+  // 기존파일도 업데이트해줘야한다.
+});
+
+router.put("/:id/option", async (req, res) => {
+  // 수행해야할 로직
+
+  // 옵션을 업데이트한다.
+  const transaction = await sequelize.transaction();
+  try {
+    // 옵션들 다 지우기
+    console.log(req.params);
+    const del = await Option.destroy(
+      {
+        where: {
+          room_id: req.params.id,
+        },
+      },
+      { transaction: transaction }
+    );
+    console.log(del);
+    const { options } = req.body;
+
+    console.log(options);
+    for (option of options) {
+      await Option.create({
+        item: option,
+        room_id: req.params.id,
+      });
+    }
+
+    return res.json({ update: true });
+  } catch (error) {
+    await transaction.rollback();
+    console.log(error);
+    return res.json({ update: false });
+  }
+});
+
+router.put("/:id/image", upload.array("files"), async (req, res) => {
+  // 파일읽어와서 지우고 파일도 지우고
+  // 새로 파일도 읽어와서 업데이트하고
+  const transaction = await sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const files = await Image.findAll({
+      where: {
+        room_id: id,
+      },
+    });
+    console.log(files);
+
+    // 주소 저장하기
+    const srcs = files.map((li) => li.dataValues.src);
+    // 삭제하기
+    srcs.forEach((li) => {
+      fs.unlink(li, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+
+    // 지우기
+    const deleteImages = await Image.destroy({
+      where: {
+        room_id: id,
+      },
+    });
+    console.log(deleteImages);
+    // 추가하기
+    if (req.files) {
+      await Promise.all(
+        req.files.map(async (li) => {
+          await Image.create(
+            {
+              src: li.path,
+              room_id: id,
+            },
+            { transaction: transaction }
+          );
+        })
+      );
+    }
+    console.log(srcs);
+    transaction.commit();
+    return res.json({update: true})
+  } catch (error) {
+    await transaction.rollback();
+    return res.json({update: false});
+  }
+});
 
 router.post("/", upload.array("files"), async (req, res) => {
   const transaction = await sequelize.transaction();
